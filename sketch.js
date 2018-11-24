accdefcfea5fc699d9985ac4705c51560fee112f
asharron <alexsharron@gmail.com>
2018-11-24 09:19:38 -0800

    Added quick hack to prevent canvas from overfilling

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 var player;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  //Quick hack to prevent the canvas from overfilling the viewport
+  createCanvas(windowWidth * .98, windowHeight * .98);
   player = new Player();
 }
 
 function draw() {
   clear();
   player.render();
+}
+
+//Called whenever the user resizes the window
+function windowResized(){
+  resizeCanvas(windowWidth * .98, windowHeight * .98);
 }
